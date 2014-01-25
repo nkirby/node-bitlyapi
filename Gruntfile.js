78afa1f8f45d0936eef5bcee02c5311dd2c4e1e2
Nathaniel Kirby <nkirby@optimo-it.com>
2014-01-25 00:08:59 -0500

    Cleanup Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,12 @@
 // ================================================
-// node-bitly
+// node-bitlyapi
+// Grunt build file for local development. Including this in the repo
+// in case people really want to build themselves a node module from
+// the CoffeeScript provided.
 // ================================================
 
 module.exports = function(grunt) {
 	grunt.initConfig({
-
-// ================================================
-// CoffeeScript
-// ================================================
-
 		coffee: {
 			bitly: {
 				options: {
